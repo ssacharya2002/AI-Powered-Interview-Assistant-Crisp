5104a6309ab2065151c0bcbc060577ff1d231d1f
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2025-09-29 07:35:55 +0530

    feat: Toaster added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,14 +6,16 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store/store.ts";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <BrowserRouter>
+          <App />
+          <Toaster />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </StrictMode>
